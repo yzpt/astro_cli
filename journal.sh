076e4f7077403b27403eeaeb524c217f2e8057ba
Yohann Zapart <yzpt.de@gmail.com>
2023-10-17 16:10:09 +0200

    -

---
+++ journal.sh
@@ -0,0 +1,14 @@
+# ================ ASTRO CLI =========================================================================
+# Marc Lamberti :
+# https://marclamberti.com/blog/getting-started-airflow/
+# https://docs.astronomer.io/astro/cli/install-cli?tab=windowswithwinget#install-the-astro-cli
+# https://www.youtube.com/watch?v=xUKIL7zsjos&ab_channel=DatawithMarc
+
+astro dev init
+astro dev start --wait 2m
+astro dev restart
+astro dev stop
+
+
+
+
